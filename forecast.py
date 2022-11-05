47edd8c6f906dcb1782a677edfaf3975bddc340f
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-11-05 16:50:15 +0700

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -34,12 +34,6 @@ def _max_width_():
 st.set_page_config(page_icon="📊", page_title="Forecast",layout="wide")
 
 
-
-
-
-
-
-
 ###################################
 
 with st.sidebar:
@@ -96,7 +90,7 @@ with st.sidebar:
 
 
 st.subheader('2. Data loading 📋')
-st.write("Your raw data will show here.")
+st.write("Your data will show here.")
 #st.write(shows)
 
 #########################################
@@ -155,87 +149,37 @@ response = AgGrid(
 
 
 st.subheader('3. Forecast')
-col1, col2 = st.columns([1,1])
-with col1:
-    with st.container():
-        gb = GridOptionsBuilder.from_dataframe(shows2)
-        #gb.configure_default_column(shows2.columns[0])
-        gb.configure_column(shows2.columns[0], rowGroup=True)
-        ###
 
-        #gb.configure_column(shows2['Material'],headerCheckboxSelection=False)
-        gb.configure_selection(selection_mode="single",use_checkbox=True)
-        
-        #gb.configure_side_bar()
-        gb.configure_columns(shows2.columns.values.tolist(),headerCheckboxSelection=False, editable=True)
-        js = JsCode("""
-                    function(e) {
-                        let api = e.api;
-                        let rowIndex = e.rowIndex;
-                        let col = e.column.colId;
-
-                        let rowNode = api.getDisplayedRowAtIndex(rowIndex);
-                        api.flashCells({
-                          rowNodes: [rowNode],
-                          columns: [col],
-                          flashDelay: 10000000000
-                        });
-
-                    };
-                    """)
-
-        gb.configure_grid_options(onCellValueChanged=js)
-        gridOptions = gb.build()
-        
-        st.markdown("""
-                    ###
-                    Edited cells are highlighted
-                    """)
-
-        #response = AgGrid(shows2, gridOptions=gridOptions, key=shows2.columns[0], allow_unsafe_jscode=True, reload_data=False)
-        
-        
-  
-        response = AgGrid(
-            shows2,
-            gridOptions=gridOptions,
-            allow_unsafe_jscode=True, reload_data=False,
-            enable_enterprise_modules=True,
-            update_mode=GridUpdateMode.MODEL_CHANGED,
-            data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-            fit_columns_on_grid_load=True,
-        )
-
-with col2:
-    model = st.multiselect(
-    'Choose your forecast model',
-    ['UCM', 'SARIMAX', 'Prophet', 'Holt-Winter'],
-    ['Holt-Winter'])
+model = st.multiselect(
+'Choose your forecast model',
+['UCM', 'SARIMAX', 'Prophet', 'Holt-Winter'],
+['Holt-Winter'])
 
 
-    df = pd.DataFrame(response["selected_rows"])
-    df = pd.melt(df,id_vars=shows.columns[0])
-    df.rename({'variable': 'Date'}, axis=1, inplace=True)
-    df['Date'] = df['Date'].apply(lambda x: datetime.strptime("01-{}".format(x),"%d-%m-%Y").date())
-    df = pd.DataFrame(df.pivot('Date','Material','value'))
-    df.index = pd.to_datetime(df.index)
-    if 'Holt-Winter' in model:
-        df_HW = md.HoltWinter(df)
-        df = df.merge(df_HW,left_index=True,right_index=True,how='outer',indicator=True)
-    if 'SARIMAX' in model:
-        df_SARIMAX = md.SARIMAX(df)
-        df = df.merge(df_SARIMAX,left_index=True,right_index=True,how='outer',indicator=True)
-    if 'UCM' in model:
-        df_UCM = md.UCM(df)
-        df = df.merge(df_UCM,left_index=True,right_index=True,how='outer',indicator=True)
-    
-    
-    df.drop(['_merge'],axis=1,inplace=True)
-    df = df.apply(pd.to_numeric)
-    
-    #df.sort_values(by=['Material','Date'],inplace=True)
-    
-    st.line_chart(df)
+df = pd.DataFrame(response["selected_rows"])
+df = df.drop(['Group','Description'], axis=1)
+df = pd.melt(df,id_vars=shows.columns[0])
+df.rename({'variable': 'Date'}, axis=1, inplace=True)
+df['Date'] = df['Date'].apply(lambda x: datetime.strptime("01-{}".format(x),"%d-%m-%Y").date())
+df = pd.DataFrame(df.pivot('Date','Material','value'))
+df.index = pd.to_datetime(df.index)
+if 'Holt-Winter' in model:
+    df_HW = md.HoltWinter(df)
+    df = df.merge(df_HW,left_index=True,right_index=True,how='outer',indicator=True)
+if 'SARIMAX' in model:
+    df_SARIMAX = md.SARIMAX(df)
+    df = df.merge(df_SARIMAX,left_index=True,right_index=True,how='outer',indicator=True)
+if 'UCM' in model:
+    df_UCM = md.UCM(df)
+    df = df.merge(df_UCM,left_index=True,right_index=True,how='outer',indicator=True)
+
+
+df.drop(['_merge'],axis=1,inplace=True)
+df = df.apply(pd.to_numeric)
+
+#df.sort_values(by=['Material','Date'],inplace=True)
+
+st.line_chart(df)
     
     
 

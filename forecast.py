8b485da1838cdcbf577d3131e852458c7569ddbd
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-11-05 15:35:44 +0700

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -109,8 +109,8 @@ with col1:
         gb = GridOptionsBuilder.from_dataframe(shows2)
         gb.configure_column(shows2.columns[0], rowGroup=True)
         ###
-        #gb.configure_default_column(enablePivot=False, enableValue=True, enableRowGroup=False,editable=True)
-        gb.configure_column(shows2.columns[1],headerCheckboxSelection=False)
+        #gb.configure_default_column(shows2.columns[1],enablePivot=False, enableValue=False, enableRowGroup=False,editable=False)
+        gb.configure_column(shows2['Material'],headerCheckboxSelection=False)
         gb.configure_selection(selection_mode="single",use_checkbox=True)
         
         #gb.configure_side_bar()

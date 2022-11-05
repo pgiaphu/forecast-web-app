c84d367683e04b9126152bd59c0dfdc8a1513e75
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-11-05 16:09:50 +0700

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -96,10 +96,61 @@ with st.sidebar:
 
 st.subheader('2. Data loading 📋')
 st.write("Your raw data will show here.")
-st.write(shows)
+#st.write(shows)
+
+#########################################
+
+gb = GridOptionsBuilder.from_dataframe(shows2)
+gb.configure_default_column(shows2.columns[0])
+#gb.configure_column(shows2.columns[0], rowGroup=True)
+###
+
+#gb.configure_column(shows2['Material'],headerCheckboxSelection=False)
+gb.configure_selection(selection_mode="single",use_checkbox=True)
+
+#gb.configure_side_bar()
+gb.configure_columns(shows2.columns.values.tolist(),headerCheckboxSelection=False, editable=True)
+js = JsCode("""
+            function(e) {
+                let api = e.api;
+                let rowIndex = e.rowIndex;
+                let col = e.column.colId;
+
+                let rowNode = api.getDisplayedRowAtIndex(rowIndex);
+                api.flashCells({
+                  rowNodes: [rowNode],
+                  columns: [col],
+                  flashDelay: 10000000000
+                });
+
+            };
+            """)
+
+gb.configure_grid_options(onCellValueChanged=js)
+gridOptions = gb.build()
+
+st.markdown("""
+            ###
+            Edited cells are highlighted
+            """)
+
+#response = AgGrid(shows2, gridOptions=gridOptions, key=shows2.columns[0], allow_unsafe_jscode=True, reload_data=False)
+
+
+response = AgGrid(
+    shows2,
+    gridOptions=gridOptions,
+    allow_unsafe_jscode=True, reload_data=False,
+    enable_enterprise_modules=True,
+    update_mode=GridUpdateMode.MODEL_CHANGED,
+    data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+    fit_columns_on_grid_load=True,
+)
 
 
 
+
+#########################################
 from st_aggrid import GridUpdateMode, DataReturnMode
 
 st.subheader('3. Forecast')

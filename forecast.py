f48eea5de7bef5d9f26aa76af464359674accf92
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-11-09 22:12:34 +0700

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -143,7 +143,7 @@ model = st.multiselect(
 
 
 df = pd.DataFrame(response["selected_rows"])
-st.write(response["selected_rows"])
+st.write(response["selected_rows"][0])
 st.write(df.iloc[0,df.columns.get_loc('Material')])
 df = df.drop(['Group','Description'], axis=1)
 df = pd.melt(df,id_vars=df.columns[0])

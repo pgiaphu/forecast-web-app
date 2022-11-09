88e94b22fad1e4285ae63c5abb0f739675d1cc94
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-11-09 21:55:30 +0700

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -52,8 +52,8 @@ with st.sidebar:
     
 ###################################
     if uploaded_file is not None:
-        shows = pd.read_excel(uploaded_file, sheet_name = "Full History")
-        shows = shows.fillna(0)
+        df_as = pd.read_excel(uploaded_file, sheet_name = "Full History")
+        df_as = df_as.fillna(0)
         df_forecast = pd.read_excel(uploaded_file, sheet_name = "Forecast")
         
         st.info(
@@ -81,16 +81,16 @@ st.write("Your baseline forecast will show here.")
 
 #########################################
 
-gb = GridOptionsBuilder.from_dataframe(shows)
+gb = GridOptionsBuilder.from_dataframe(df_as)
 #gb.configure_default_column(shows2.columns[0])
-gb.configure_column(shows.columns[0], rowGroup=True)
+gb.configure_column(df_as.columns[0], rowGroup=True)
 ###
 
 #gb.configure_column(shows2['Material'],headerCheckboxSelection=False)
 gb.configure_selection(selection_mode="single",use_checkbox=True)
 
 #gb.configure_side_bar()
-gb.configure_columns(shows.columns.values.tolist(),headerCheckboxSelection=False, editable=True)
+gb.configure_columns(df_as.columns.values.tolist(),headerCheckboxSelection=False, editable=True)
 js = JsCode("""
             function(e) {
                 let api = e.api;

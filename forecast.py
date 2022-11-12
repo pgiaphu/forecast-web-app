71228503052bf5b8f6fc96c8398bf2e13b81cd70
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-11-12 12:06:32 +0700

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -190,13 +190,13 @@ df = pd.concat([df,df_baseline,df_HW,df_SARIMAX,df_UCM])
 #df.sort_values(by=['Material','Date'],inplace=True)
 dfplot = df.copy().reset_index()
 #dfplotmonth = dfplot.iloc[:,:][dfplot.Model == 'Actual'][sku]
-st.write(dfplot.iloc[:,:][dfplot.Model == 'Actual'][sku])
+#st.write(dfplot.iloc[:,:][dfplot.Model == 'Actual'][sku])
 
 #dfplot.index = pd.to_datetime(dfplot.Date)
 fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(8, 4))
 sns.set_theme(style="whitegrid", palette="pastel")
 sns.lineplot(data=dfplot,x="Date", y=dfplot.columns[1],hue='Model',ax=ax[0])
-month_plot(dfplot.iloc[:,:][dfplot.Model == 'Actual'][sku],dfplot.Date,ax=ax[1])
+month_plot(dfplot.iloc[:,:][dfplot.Model == 'Actual'][sku],dfplot.iloc[:,:][dfplot.Model == 'Actual']['Date'],ax=ax[1])
 
 #plt.ylim(0,2000)
 st.pyplot(fig)

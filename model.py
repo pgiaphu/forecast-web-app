d68708567174bdcfdd35793f669769929bb5bb54
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2023-01-06 12:10:23 +0700

    Update model.py

--- model.py
+++ model.py
@@ -143,12 +143,12 @@ def UCM(df: pd.DataFrame,f=0,ar=0,ucmmodel='ntrend'):
       future_index = []
       future_index.append(df.tail(12).index.shift(12,freq="MS"))
       UCM_param_gridsearch = {  
-        'level': ['ntrend','dconstant','llevel','rwalk','dtrend','lldtrend','rwdrift','lltrend','strend','rtrend'],
+        'level': ['ntrend','lldtrend','lltrend','strend','rtrend'],
         'cycle': [True,False],
         'irregular': [True,False],
         'damped_cycle': [True,False],
         'use_exact_diffuse': [True,False],
-        'autoregressive': [1,2]
+        #'autoregressive': [1,2]
                         }
       UCM_all_params = [dict(zip(UCM_param_gridsearch.keys(), v)) for v in itertools.product(*UCM_param_gridsearch.values())]
       for sku in df.columns:

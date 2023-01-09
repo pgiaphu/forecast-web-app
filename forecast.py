dc01a34e40e46ebabef88370ec477b3b53e5f398
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2023-01-09 17:04:25 +0700

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -279,7 +279,7 @@ with col1:
                     'tree_method': tree_method,
                     'max_leaves': max_leaves
                         }
-            df_XGB, df_LGBM = md.ML_FC(df,model='XGB',select_type=select_type,param_gridsearch)
+            df_XGB, df_LGBM = md.ML_FC(df,model='XGB',select_type='Manual',param_gridsearch)
         else:
             df_XGB, df_LGBM = md.ML_FC(df)
             

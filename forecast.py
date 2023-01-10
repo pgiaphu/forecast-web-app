92154151465dc8f58260f3d9c86a50a5b3fa9e87
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2023-01-10 13:26:43 +0700

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -281,8 +281,8 @@ with col1:
                     'max_leaves': max_leaves
                         }
             '''
-            #df_XGB = md.ML_FC(df,model='XGB',select_type='Auto',learning_rate=learning,max_depth=maxdep,n_estimators=n,tree_method=tree,max_leaves=maxlea)
-            df_XGB = md.time_features(df)
+            df_XGB = md.ML_FC(df,model='XGB',select_type='Auto',learning_rate=learning,max_depth=maxdep,n_estimators=n,tree_method=tree,max_leaves=maxlea)
+            #df_XGB = md.time_features(df)
             st.write(df_XGB)
         else:
             df_XGB = md.ML_FC(df)

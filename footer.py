d4a7f8ec9d84731d2230a738fcf83cb7d706d291
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2023-01-11 15:42:10 +0700

    Update footer.py

--- footer.py
+++ footer.py
@@ -20,7 +20,7 @@ def layout(*args):
         margin=px(2, 2, 2, 2),
         width=percent(100),
         color="black",
-        text_align="center",
+        text_align="left",
         height=5,
         opacity=1
     )
@@ -53,22 +53,11 @@ def layout(*args):
 
     st.markdown(str(foot), unsafe_allow_html=True)
 
-#############
-footer="""
-
-<style> your css code put here</style>
-
-<div class='footer'>
-
-<p>If you need support, reach out to PhamGiaPhu@duytan.com or call ☎:240<a style='display:block;text-align:center;' 
-
-href='https://www.streamlit.io' target='_blank'>phamgiaphu@duytan.com</a></p>
-
-</div>"""
 
 
-#############
 def footer():
-    #myargs = ["©: PhamGiaPhu@duytan.com    |    ☎: 240    |    Dept: Supply Chain Planning    |    Manager: PhamHungTinh@duytan.com"]
-    #layout(*myargs)
-    st.markdown(footer, unsafe_allow_html=True)
+    myargs = ["""If you need support, 
+                    reach out to PhamGiaPhu@duytan.com  
+                    or call ☎:240"""]
+    layout(*myargs)
+   

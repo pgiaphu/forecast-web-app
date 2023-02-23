90851066942229dc83b8911d5af777096ac50f0c
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2023-02-23 11:03:05 +0700

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -40,7 +40,7 @@ with st.sidebar:
     )    
     st.title("TIME SERIES FORECAST")
     st.title("1. Select Data")
-    data_options = st.checkbox("Load sample data", False)
+    data_options = st.checkbox("Load sample data", True)
     if data_options == True:
         uploaded_file = "AS.xlsx"
     else:
